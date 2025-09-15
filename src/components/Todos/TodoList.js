0ae5398a7fa1947364c8aec509d8a5c6dc704830
Parvani Vafa <porvanivafoo@gmail.com>
2025-09-16 01:13:04 +0600

    logic for empty list

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -4,6 +4,7 @@ import styles from './TodoList.module.css'
 function TodoList({ todos }) {
   return (
     <div className={styles.todoListContainer}>
+      {!todos.length === 0 && <h2>Todo list is empty</h2>}
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} />
       ))}
