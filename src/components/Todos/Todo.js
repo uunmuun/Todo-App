2bf754a864e98b0e42afe45d4d7b7d633d667842
Parvani Vafa <porvanivafoo@gmail.com>
2025-09-16 01:04:28 +0600

    added some css styles

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,5 +1,11 @@
+import styles from './Todo.module.css'
+
 function Todo({ todo }) {
-  return <h1>{todo}</h1>
+  return (
+    <div className={styles.todo}>
+      <div className={styles.todoText}>{todo}</div>
+    </div>
+  )
 }
 
 export default Todo
