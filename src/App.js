d6107a2c25840d90dee257320921d638bb68cb9a
Parvani Vafa <porvanivafoo@gmail.com>
2025-09-15 21:47:09 +0600

    adding folders and the initial code for the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import TodoForm from './components/Todos/TodoForm'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <TodoForm />
+      <TodoForm />
+    </div>
+  )
 }
 
 export default App
